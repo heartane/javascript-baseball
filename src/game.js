7326b3145690fb612b27b4203f4accfa54cda76a
Jin garam <2001garam@naver.com>
2022-11-05 16:37:34 +0900

    bug(printGameOver): return 부재로 인한 오류 수정
    return이 없어서 해당 함수를 호출한 후 돌아와 다음 라인으로 이동하면서 에러발생

--- src/game.js
+++ src/game.js
@@ -25,7 +25,7 @@ function printGameOver() {
     '3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
     (answer) => {
       const { restart, close } = SIGN;
-      if (parseInt(answer) === restart) playRoutine(randomGoalNumber());
+      if (parseInt(answer) === restart) return playRoutine(randomGoalNumber());
       if (parseInt(answer) === close) Console.close();
       else throw new Error('입력값 오류');
     }
@@ -65,3 +65,7 @@ function matchNumbers(goal, subject) {
 function setMatchResult(map, match_status) {
   return map.set(match_status, map.get(match_status) + 1 || 1);
 }
+
+module.exports = {
+  playRoutine,
+};
